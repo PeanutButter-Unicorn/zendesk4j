e1a14f04918bf89306adf46fffdb5c62071cbc67
jonathan zollinger <jonathan.zollinger@outlook.com>
2025-05-05 22:45:58 -0600

    feat: introduce userSegmentClient support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,56 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
-    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
-    id("groovy") 
+    id("groovy")
     id("io.micronaut.application") version "4.5.3"
-    id("com.gradleup.shadow") version "8.3.6"
+    id("it.nicolasfarabegoli.conventional-commits") version "3.1.3"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
     id("io.micronaut.aot") version "4.5.3"
 }
 
-version = "0.1"
+version = project.properties["zendesk4jVersion"]!!
 group = "lol.pbu"
 
-apply(from="gradle/asciidoc.gradle")
-val kotlinVersion=project.properties.get("kotlinVersion")
+apply(from = "gradle/asciidoc.gradle")
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    ksp("io.micronaut.data:micronaut-data-processor")
-    ksp("io.micronaut:micronaut-http-validation")
-    ksp("io.micronaut.serde:micronaut-serde-processor")
+    annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
+    implementation("io.micronaut.reactor:micronaut-reactor-http-client")
     implementation("io.micronaut:micronaut-http-client")
     implementation("io.micronaut.data:micronaut-data-jdbc")
-    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
     implementation("io.micronaut.serde:micronaut-serde-jackson")
     implementation("io.micronaut.sql:micronaut-jdbc-hikari")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     implementation("org.slf4j:jul-to-slf4j")
+    implementation("io.micronaut.validation:micronaut-validation")
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.yaml:snakeyaml")
     testImplementation("org.testcontainers:spock")
     testImplementation("org.testcontainers:testcontainers")
+    testImplementation("net.datafaker:datafaker:2.4.3")
 }
 
 
 application {
-    mainClass = "lol.pbu.ApplicationKt"
+    mainClass = "lol.pbu.Application"
 }
 java {
-    sourceCompatibility = JavaVersion.toVersion("21")
+    sourceCompatibility = JavaVersion.toVersion("17")
 }
 
+fun loadEnv() {
+    if (file(".env").exists())
+        file(".env").readLines().forEach {
+            val (key, value) = it.split("=", limit = 2)
+            System.setProperty(key.trim(), value.trim())
+        }
+}
 
-graalvmNative.toolchainDetection = false
+tasks.withType<Test> {
+    loadEnv()
+}
 
 micronaut {
     runtime("netty")
@@ -69,10 +72,3 @@ micronaut {
         replaceLogbackXml = true
     }
 }
-
-
-tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
-    jdkVersion = "21"
-}
-
-
